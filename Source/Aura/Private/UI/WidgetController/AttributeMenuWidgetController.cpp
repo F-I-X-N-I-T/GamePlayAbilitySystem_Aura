0a7e896ad8ad38b0fe1ca53494ae63958ffbc405
Angel Gustavo <tavomx.987@gmail.com>
2023-10-09 16:14:55 -0600

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright FIXNIT GAMES
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
